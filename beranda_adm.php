a549c44157ecd3af15fd03a0bddbe2a6ffef9042
evirmdni <60312734+evirmdni@users.noreply.github.com>
2020-01-26 16:27:48 +0700

    Add files via upload

---
+++ beranda_adm.php
@@ -0,0 +1,40 @@
+<div class="container">
+    <div class="row">
+        <div class ="col-xs-12">
+
+            <div class="alert alert-info">
+                Selamat datang kembali <strong><?=$_SESSION['nama']?></strong>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <!--colomn kedua-->
+        <div class="col-sm-12 col-xs-12">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h3 class="panel-title">Halaman Administrator Sistem Informasi Pencatatan Barang Masuk Pada PT.PELINDO 1 (PERSERO) </h3>
+                </div>
+                <div class="panel-body">
+                     <table id="dtskripsi" class="table table-bordered table-striped table-hover">
+                       <thead>
+                          <p align="center"><img src="img/1.png" width="500px" height="250px">
+
+
+                          </p> 
+
+                          <p class="label-title" align="center"><strong>PT.PELINDO 1 (PERSERO) TANJUNGBALAI</strong></p>
+                          <p class="col-sm-12 col-xs-12" align="center">
+                            Selamat datang di halaman administrator Sistem Informasi Pencatatan Barang Masuk Pada PT.PELINDO 1 (PERSERO) TANJUNGBALAI.<br>
+                            Melalui halaman ini dapat dilakukan pengelolaan data barang masuk di gudang.<br><br>
+                            Akses menu Master Data pada bagian atas sistem untuk pengelolaan data barang masuk.<br>
+                            Untuk mengelola laporan, dapat dilakukan dengan mengakses menu report pada bagian atas sistem.<br>
+                            Akses menu User untuk mengelola informasi tentang user yang login.<br><br><br>
+
+                          </p>
+
+                         </thead>
+                         </table>
+                </div>
+            </div>
+        </div>
+</div>
